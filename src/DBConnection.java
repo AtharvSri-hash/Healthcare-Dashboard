fd0b74257ac556624077efa21307629a5b14b373
Kavyansh Vats <kavyansh1509@gmail.com>
2026-04-05 10:14:23 +0530

    Added Updated Database part in git repo

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,17 +1,26 @@
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DBConnection {
     public static Connection getConnection() {
         try {
-            // Force load MySQL driver
+            // Load properties file
+            Properties props = new Properties();
+            FileInputStream fis = new FileInputStream("db.properties");
+            props.load(fis);
+
+            // Get values
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            // Load MySQL driver
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/healthcare_dashboard",
-                "root",
-                "Kavu@2004V"   // your password
-            );
+            // Connect
+            Connection conn = DriverManager.getConnection(url, user, password);
 
             System.out.println("Connected to MySQL!");
             return conn;
